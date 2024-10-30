2f1644a30174ae6b41861d90b5dabadc4c5f4a4d
hassan14545 <kamrul.hassan@devotrixinc.com>
2024-10-30 12:27:52 +0600

    w

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <HeaderComponent />
-      <TopSection />
+      {/* <TopSection /> */}
       <NoticeSection />
       <CollectionSection />
       <CollectionFeatures />
